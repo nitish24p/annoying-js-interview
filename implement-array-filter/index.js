f41f6d8953d0f161feb621f93c2f84fe6feba11d
NitishPhanse <nitsums@gmail.com>
2019-04-16 22:36:34 +0530

    implement array filter

---
+++ implement-array-filter/index.js
@@ -0,0 +1,27 @@
+/**
+ * Filter function is attached to the array prototype
+ * It takes in 3 parameters, callback and a context (this)
+ * argument, the callback is invoked with the current item
+ * the index, the array
+ */
+
+/**
+ * The callback gets 3 arguments
+ * the item, the index and the  current array being traversed
+ */
+Array.prototype.myFilter = function (callback, context) {
+  const result = [];
+  for (let index = 0; index < this.length; index++) {
+    if (callback.call(context, this[index], index, this)) {
+      result.push(this[index])
+    }
+  }
+
+  return result
+}
+
+// example
+const numbers = [1, 2, 3, 4]
+const even = numbers.myFilter(item => item % 2 === 0)
+
+console.log(even)
