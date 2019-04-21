58373f05520e8659639da6eaa94ce038388b7f3f
NitishPhanse <nitsums@gmail.com>
2019-04-22 00:25:02 +0530

    changed example

--- implement-bind/index.js
+++ implement-bind/index.js
@@ -1,15 +1,26 @@
 Function.prototype.newBind = function (context) {
   const currentContext = this;
-  const currentArguments = Array.prototype.slice.call(arguments);
+  const currentArguments = Array.prototype.slice.call(arguments, 1); // Dont need the context
   return function () {
-    const args = Array.slice.call(arguments);
+    const args = Array.prototype.slice.call(arguments);
     currentContext.apply(context, currentArguments.concat(args))
   }
 }
 
-function Hello(firstName, lastname) {
-  console.log("hello", firstName, lastname)
-}
+this.x = 9;    // this refers to global "window" object here in the browser
+const module = {
+  x: 81,
+  getX: function () { return this.x; }
+};
 
-const newHello = Hello.bind(this, "nitish");
-newHello("phanse")
+module.getX(); // 81
+
+const retrieveX = module.getX;
+retrieveX();
+// returns 9 - The function gets invoked at the global scope
+
+// Create a new function with 'this' bound to module
+// New programmers might confuse the
+// global const x with module's property x
+const boundGetX = retrieveX.bind(module);
+console.log(boundGetX()); // 81
